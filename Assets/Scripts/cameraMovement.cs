fc774e27946b37b6e90933fe3aa6e05d89338d6f
Axel Rodríguez <A01570152@itesm.mx>
2022-07-28 14:41:04 -0500

    Light and movement

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -3,9 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraMovement:MonoBehaviour{
+    float speedH = 2.0f, speedV = 2.0f;
+    float yaw = -90.0f, pitch = 10.0f;
+
     void Update(){
-        if(Input.GetButton("Jump")){
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Input.GetAxis("Vertical") * -30.0f + 10.689f, Input.GetAxis("Horizontal") * 60.0f - 90.149f, -0.045f),  Time.deltaTime * 5.0f);
+        /* if(Input.GetButton("Jump")){
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Input.GetAxis("Vertical") * -30.0f + 10.0f, Input.GetAxis("Horizontal") * 60.0f - 90.0f, 0.0f),  Time.deltaTime * 5.0f);
+        } */
+        if(Input.GetButton("Fire1")){
+           /*  if(Input.GetButton("Vertical")){
+                transform.Rotate(transform.rotation.x + Input.GetAxis("Vertical") * -0.5f, 0.0f, 0.0f);
+            }
+            else if(Input.GetButton("Horizontal")){
+                transform.Rotate(0.0f, transform.rotation.y + 1.0f, 0.0f);
+            } */
+            yaw += speedH * -Input.GetAxis("Mouse X");
+            pitch -= speedV * -Input.GetAxis("Mouse Y");
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+        if(Input.GetButton("Vertical")){
+            transform.position += new Vector3(Input.GetAxis("Vertical") * -0.5f, 0.0f, 0.0f);
+        }
+        else if(Input.GetButton("Horizontal")){
+            transform.position += new Vector3(0.0f, 0.0f, Input.GetAxis("Horizontal") * 0.5f);
         }
     }
 }
--- Assets/Scripts/ligthMovement.cs
+++ Assets/Scripts/ligthMovement.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ligthMovement:MonoBehaviour{
-    float elapsed = 0f, rot = 0f;
-    void Update(){
+    /* float elapsed = 0f, rot = 45f;
+    void FixedUpdate(){
         elapsed += Time.deltaTime;
         if (elapsed >= 1f) {
             elapsed = elapsed % 1f;
-            transform.position += new Vector3(0.0f, 0.0f, 5.0f);
-            rot += 0.9f;
+            transform.position += new Vector3(0.0f, 0.0f, 10.0f);
+            rot += 1f;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rot, -23.45f, 0.0f),  Time.deltaTime * 5.0f);
         }
+    } */
+    float rot = 30.0f, intensity = 5.0f;
+    void Update(){
+        intensity += 0.001f;
+        this.gameObject.GetComponent<Light>().intensity = intensity;
+        if(transform.position.y < 1300.0f){
+            transform.position += new Vector3(0.0f, 0.4f, 0.0f);
+        }
+        transform.position += new Vector3(0.0f, 0.0f, 0.2f);
+        rot += 0.025f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rot, -23.45f, 0.0f),  Time.deltaTime * 5.0f);
+        if(transform.position.z > 500.0f){
+            resetLight();
+        }
+    }
+
+    void resetLight(){
+        rot = 30.0f;
+        intensity = 5.0f;
+        transform.position = new Vector3(0.0f, 0.0f, -500.0f);
+        transform.rotation = Quaternion.Euler(30.0f, -23.45f, 0.0f);
     }
 }
